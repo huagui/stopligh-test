431c21acaee8e4eff18a6b1038d95b4f223ed45c
hugh <953991592@qq.com>
2025-03-05 12:22:42 +0800

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NDU0OQ/spectral.js?branch_slug=master&token=f5daee93-139a-45f4-a7bb-39b8b171e6fd";
+export default { extends: ruleset };
