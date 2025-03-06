2946cae71421f2609642debb2c06a5c6841c77a2
hugh <953991592@qq.com>
2025-03-06 13:45:25 +0800

    Update .spectral.mjs

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,2 +1,5 @@
 import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NDU0OQ/spectral.js?branch_slug=test&token=f5daee93-139a-45f4-a7bb-39b8b171e6fd";
 export default { extends: ruleset };
+
+
+//123
